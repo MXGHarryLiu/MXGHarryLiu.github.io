6813a82ef285b6a45dde88a601a49d576cf5b917
MXGHarryLiu <harry.liu@rice.edu>
2023-01-06 10:55:06 -0600

    v230106
    Add dynamic last updated date.

---
+++ js/lastupdated.jsx
@@ -0,0 +1,30 @@
+/**
+  https://stackoverflow.com/questions/29845346/how-to-get-latest-commit-date-github-api
+ */
+class LatestCommitComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {date: ""};
+    }
+  
+    componentDidMount() {
+      fetch("https://api.github.com/repos/MXGHarryLiu/MXGHarryLiu.github.io/branches/main")
+        .then(response => {
+          response.json().then(json => {
+            console.log(json);
+            this.setState({date: json.commit.commit.author.date});
+          });
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  
+    render() {
+      return (
+        <a class="plain-text footer">Last updated: {this.state.date.slice(0, 10)}</a>
+      );
+    }
+  }
+  
+  ReactDOM.render(<LatestCommitComponent/>, document.getElementById("lastupdated"));
